3405d9b2c7abc8e8c63fefde17df478a648fc735
Patrick-Bell <144367992+Patrick-Bell@users.noreply.github.com>
2024-07-14 22:26:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
-                 
+                  
 const geoLocationApiKey = 'AIzaSyAMHSff5psiIahOTIauOeEfZ931odqYFY0'
 const placesApiKey = 'AIzaSyCVCeWXKFZ2_esgya8qX2yL3JNmGttm0tk'
  
